05d079e21acc9c56bb30b4fedd4d0113dbc1241c
Prabesh Baskota <80308797+s333608@users.noreply.github.com>
2026-02-07 19:49:52 +0930

    Update Vite config to set base path
    Removed server and build configurations, added base path.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    port: 5173,
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'terser'
-  }
-})
+export default {
+  base: '/Prabesh/',
+}
